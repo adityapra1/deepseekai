d510b478e4bdb7b337515047ee52a61414923e75
adityapra1 <adityaprakash.shibu@gmail.com>
2026-07-05 14:46:09 +0530

    Updated backend URL in utils

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,2 @@
-// Aap isko ek config.js ya constants.js file mein rakh sakte hain
-export const BACKEND_URL = import.meta.env.MODE === "development" 
-  ? "http://localhost:4002/api/v1" // Aapka Local URL
-  : "https://deepseek-backend-app.onrender.com/api/v1"; // Aapka Render URL
+
+export const BACKEND_URL = "https://deepseekai-8ij6.onrender.com/api/v1";
